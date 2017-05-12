308882c8492bad521b1941ed1fd25bc18880e32a
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2017-05-12 12:51:56 +0100

    Update vSAN-Change-Adv-Settings.py

--- vSAN-Change-Adv-Settings.py
+++ vSAN-Change-Adv-Settings.py
@@ -2,6 +2,10 @@ from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import Cache
 import ssl
 import atexit
+from pyVmomi import vim
+# import the VSAN API python bindings
+
+#import the vSAN API python bindings
 
 import vsanmgmtObjects
 import vsanapiutils
@@ -31,21 +35,39 @@ print(hosts[0])
 
 print(hosts[0].configManager.advancedOption)
 
-setting = hosts[0].configManager.advancedOption.setting
+advsetting = hosts[0].configManager.advancedOption.setting
 
 # setting is type array
 
 # Loop over all the settings and look for vSAN CLomd Repair Time
 
-for i in setting:
+
+
+
+
+for i in advsetting:
 
     if i.key == "VSAN.ClomRepairDelay":
 
-        print(i)
+        print(i.key)
+        print(i.value)
+        print(sc.content.rootFolder.childEntity[0].hostFolder.childEntity[0].host[0].configManager.advancedOption.QueryOptions('VSAN.ClomRepairDelay'))
+        repair = sc.content.rootFolder.childEntity[0].hostFolder.childEntity[0].host[0].configManager.advancedOption.QueryOptions('VSAN.ClomRepairDelay')
+
+        changedValue = repair[0]
+        changedValue.key = 'VSAN.ClomRepairDelay'
+        changedValue.value = '120'
+        bn = vim.option.OptionValue(key='VSAN.ClomRepairDelay', value=120)
+        print(bn)
 
 
+# creio que tenho erro devido a estar dentro do loop
+#        TypeError: 'vim.option.OptionValue'
+#        object is not iterable
+# rever isto
+
+        sc.content.rootFolder.childEntity[0].hostFolder.childEntity[0].host[0].configManager.advancedOption.UpdateOptions(bn)
 
 
-# Close the VCenter Connection
 
 atexit.register(Disconnect, sc)
