bde17083023a87948a268ea52a7064e73d80c414
fonsen <nelson.chamusca.fonseca@gmail.com>
2017-08-14 23:23:56 +0100

    Working code with FindbyInventoryPath

--- FindVMsInventoryPath.py
+++ FindVMsInventoryPath.py
@@ -23,7 +23,7 @@ for vms in dc.vmFolder.childEntity:
 
     #print(vms.name)
     vm = si.content.searchIndex.FindByInventoryPath("GSS-VSAN-B/vm/"+vms.name)
-    # Root Folder must not be usedvs
+    # Root Folder must not be usedvsv
     print(vm.name)
 
 
