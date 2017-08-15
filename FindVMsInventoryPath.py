070281b82469c4d3e216f7f049dcdbbb2b803120
fonsen <nelson.chamusca.fonseca@gmail.com>
2017-08-15 23:07:09 +0100

    Working code with FindbyInventoryPath

--- FindVMsInventoryPath.py
+++ FindVMsInventoryPath.py
@@ -8,23 +8,20 @@ s.verify_mode = ssl.CERT_NONE
 
 si = SmartConnect(host="vcsa-gssvsan-b.csl.vmware.com", user="Administrator@vsphere.local", pwd='VMware123!', sslContext=s)
 
-datacenter = si.content.rootFolder.childEntity[0]
-
-root= si.content.rootFolder.childEntity[0]
-#print(root)
 
 
 
-# loop de todos os child entities e depois ponho dentro dos earch index
-# returns all the vm names in the Inventory
-dc = si.content.rootFolder.childEntity[0]
+dcs = si.content.rootFolder.childEntity
 
-for vms in dc.vmFolder.childEntity:
+# loop over all Datacenters and retrieve all VM's
+for dc in dcs:
 
-    #print(vms.name)
-    vm = si.content.searchIndex.FindByInventoryPath("GSS-VSAN-B/vm/"+vms.name)
-    # Root Folder must not be usedvsvf
-    print(vm.name)
+    print(dc.name)
+
+    for vms in dc.vmFolder.childEntity:
+
+        vm = si.content.searchIndex.FindByInventoryPath(dc.name+"/vm/"+vms.name)
+        print("Datacenter:"+dc.name+ " " +"VM:"+ vm.name)
 
 
 
