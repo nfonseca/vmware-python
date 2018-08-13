045bf9172a7236d5f6c69e6367a9a505cc37da3b
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2018-08-13 15:10:26 +0100

    Using Traversal Sepcs for the FIlter now.

--- Find-VM-ISOs.py
+++ Find-VM-ISOs.py
@@ -3,6 +3,7 @@
 import sys
 import ssl
 
+# Site Package Location in VXRM
 sys.path.append("/usr/lib/vmware-marvin/marvind/webapps/ROOT/WEB-INF/classes/scripts/lib/python2.7/site-packages")
 from pyVmomi import vim, vmodl
 from pyVim import connect
@@ -25,8 +26,35 @@ for child in content.rootFolder.childEntity:
     vmFolder = datacenter.vmFolder
     vmList = vmFolder.childEntity
     for vm in vmList:
-        #        print(vm.name)
-        #        print(vm.childEntity)
-        name = vm.childEntity
-        for x in name:
-            print(x.name)
+        vmname = vm.childEntity
+        for x in vmname:
+            print(x.vmname)
+
+
+
+
+# using view
+
+rootFolder = si.
+
+
+
+# Create a global view
+#
+# def get_obj(content, vimtype, name):
+#     obj = None
+#     container = content.viewManager.CreateContainerView(
+#         content.rootFolder, vimtype, True)
+#     for c in container.view:
+#         if c.name == name:
+#             obj = c
+#             break
+#     return obj
+
+
+# specs of the script
+# 1 - connect to VC
+# 2 - Find all Powered ON VMs on the DataCenter (VxRail CLusters Only)
+# 3 - Check for every of those VMs if they have an ISO Mounted
+# 4 - Report to the user a list of the VMs Found (we can eventually modify the script to remove the ISO)
+
