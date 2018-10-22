a6c5429ea748813ccafae958dfe0e05183f18e61
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2018-10-22 13:57:20 +0100

    First API test

--- vxrail-api-test.py
+++ vxrail-api-test.py
@@ -9,6 +9,14 @@ if not sys.warnoptions:
 
     warnings.simplefilter("ignore")
 
-req = requests.get('https://172.168.10.150', verify=False)
+import requests
 
-print(req)
+url = "https://172.168.10.150/rest/vxm/v1/system"
+
+response = requests.request("GET", url, verify=False, auth=('administrator@vsphere.local', 'VxR@il1!'))
+
+print(response.text)
+
+# vxrm APIS
+
+# GET https://<VxM IP>/rest/vxm/v1/system
