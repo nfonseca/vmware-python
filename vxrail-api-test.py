e8643396765731304faeed5094698d45648c2a11
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2018-10-22 16:24:01 +0100

    First API test

--- vxrail-api-test.py
+++ vxrail-api-test.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
 import sys
+from pyVmomi import vim, vmodl
+from pyVim import connect
+from datetime import datetime
+import ssl
 import requests
+import jsbeautifier
 
 # disable warnings from SSL Check
 if not sys.warnoptions:
@@ -9,13 +14,35 @@ if not sys.warnoptions:
 
     warnings.simplefilter("ignore")
 
-import requests
-
 url = "https://172.168.10.150/rest/vxm/v1/system"
 
 response = requests.request("GET", url, verify=False, auth=('administrator@vsphere.local', 'VxR@il1!'))
 
-print(response.text)
+pprint = jsbeautifier.beautify(response.text)
+
+print(pprint)
+
+######################
+# VC Connection      #
+######################
+
+s = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+s.verify_mode = ssl.CERT_NONE
+si = connect.SmartConnectNoSSL(host='172.168.10.149', user='administrator@vsphere.local', pwd='VxR@il1!')
+
+print(si.serverClock)
+
+
+# function to check if cluster is vxrail or not
+
+def isVxRailCluster():
+    return None
+
+# need to loop over all registered clusters in the DC
+# and run a check
+# returns ?
+# true if  availableField[41].name='VxRail-VERSION'
+
 
 # vxrm APIS
 
